f35286614d2e5d0b3079cdeee72b1c3f56f4b20d
Raymond Natividad <natividad2315@gmail.com>
2023-03-09 16:48:09 -0800

    Completed readings and practice assignment.

---
+++ courses/react/functionalComponents/readings/props/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = (props) => {
+    const { firstName, lastName } = props;
+    return (
+        <div>
+            <h1>
+                My name is {firstName} {lastName}
+            </h1>
+        </div>
+    );
+}
+export default Header;
