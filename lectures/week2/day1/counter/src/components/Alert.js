0f64e02415c8e957be7a82035ded0896edc65f65
Raymond Natividad <natividad2315@gmail.com>
2023-03-16 00:11:45 -0700

    Participated in lecture week2 day1.

---
+++ lectures/week2/day1/counter/src/components/Alert.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Alert = (props) => {
+    const alertButton = () => {
+        alert('you clicked me')
+    }
+    return (
+        <div>
+            <h1>Synthetic Events</h1>
+            <button onClick={alertButton}>Click me!</button>
+        </div>
+)}
+export default Alert;
