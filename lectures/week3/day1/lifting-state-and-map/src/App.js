1a0b32a326a9cbdb376c405c261a2174216c8810
Raymond Natividad <natividad2315@gmail.com>
2023-03-22 01:30:12 -0700

    participated in lecture. learned lifting state. API intro.

---
+++ lectures/week3/day1/lifting-state-and-map/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ShowForm from './components/ShowForm';
+import { useState } from 'react';
+import DisplayShows from './components/DisplayShows';
+
+function App() {
+    const [showList, setShowList] = useState([])
+    return (
+        <div className="App">
+            <ShowForm showList={showList} setShowList={setShowList}/>
+            <DisplayShows showList={showList} setShowList={setShowList}/>
+        </div>
+    );
+}
+
+export default App;
