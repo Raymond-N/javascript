31b58b36526384f24a3b69c7a239f415ae716079
Raymond Natividad <natividad2315@gmail.com>
2023-03-23 08:59:02 -0700

    Participated in Lecture. Learned Promises. Worked w/ APIs.

---
+++ lectures/week3/day2/apis-pt2/src/App.js
@@ -0,0 +1,34 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios'
+
+function App() {
+    const [starWarsCharacters, setStarWarsCharacters] = useState([])
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people/')
+            .then((response) => {
+                // console.log(`RESPONSE: ${response}`);
+                console.log('RESPONSE');
+                // console.log(response.data.results);
+                setStarWarsCharacters(response.data.results)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, [])
+    return (
+        <div className="App">
+            <h1>Using the Star Wars API!</h1>
+            <a href="https://swapi.dev/" target='_blank'>Click here to view the <b>swapi</b> documentation</a>
+            {
+                starWarsCharacters.map((character, idx) => (
+                    <div key={idx}>
+                        <h2>Name: {character.name}</h2>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default App;
