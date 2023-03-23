d0cfb3ebe6fdc28b172efa7893c899a48e79a2b4
Raymond Natividad <natividad2315@gmail.com>
2023-03-23 14:55:22 -0700

    Completed optional assignment. Coin flip. 5 heads in a row.

--- courses/react/APIs/optionalAssignments/coinFlipping.js
+++ courses/react/APIs/optionalAssignments/coinFlipping.js
@@ -21,12 +21,35 @@ function tossCoin() {
 // console.log( "This is run after the fiveHeadsSync function completes" );
 
 function fiveHeads() {
-        return new Promise( (resolve, reject) => {
-            // your code here!
-        });
-    }
-    fiveHeads()
-        .then( res => console.log(res) )
-        .catch( err => console.log(err) );
+    return new Promise( (resolve, reject) => {
+        let headsCount = 0;
+        let attempts = 0;
+        let maxAttempts = 100;
+
+        console.log(resolve);
+        console.log(reject);
+        // Need While loop to run until 5 heads is reached
+        // also max attempts cannot exceed 100
+        while(headsCount < 5 && attempts <= maxAttempts) {
+            attempts++;
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if(result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        // we need to go through loop it max attempts have not been reached
+        // run conditional checking attempts
+        if (attempts <= maxAttempts) {
+            resolve(`It took ${attempts} tries to flip five "heads" in a row`)
+        } else {
+            reject(`Stop! You exceeded ${maxAttempts} flips.`)
+        }
+    });
+}
+fiveHeads()
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
     console.log( "When does this run now?" );
-    
