6defbae1af9b9d2deca82b78098a69515aebe617
Raymond Natividad <natividad2315@gmail.com>
2023-04-13 23:54:25 -0700

    Participated in OH. Worked in createone component. Worked on errors. Did some simply styling to nav bar img.

---
+++ lectures/week6/day2/OH/CLS_Pokemon_Manager/server/controllers/pokemon.controller.js
@@ -0,0 +1,56 @@
+const Pokemon = require('../models/pokemon.model');
+
+module.exports = {
+
+    getAllPokemon: (req, res) => {
+        Pokemon.find()
+            .then((allPokemon) => {
+                res.status(200).json(allPokemon)
+                console.log(allPokemon)
+            })
+            .catch((err) => {
+                res.status(400).json(err)
+            })
+    },
+
+    createPokemon: (req, res) => {
+        Pokemon.create(req.body)
+            .then((newPokemon) => {
+                res.status(200).json(newPokemon)
+            })
+            .catch((err) => {
+                res.status(400).json(err)
+            })
+    },
+
+    getOnePokemon: (req, res) => {
+        Pokemon.findOne({_id: req.params.id})
+            .then((onePokemon) => {
+                res.status(200).json(onePokemon)
+            })
+            .catch((err) => {
+                res.status(400).json(err)
+            })
+    },
+
+    updatePokemon: (req, res) => {
+        Pokemon.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, runValidators: true})
+            .then((updatedPokemon) => {
+                res.status(201).json(updatedPokemon)
+            })
+            .catch((err) => {
+                res.status(400).json(err)
+            })
+    },
+
+    deletePokemon: (req, res) => {
+        Pokemon.deleteOne({_id: req.params.id})
+            .then(() => {
+                res.status(204).send()
+                console.log('Success! You deleted a pokemon.')
+            })
+            .catch((err) => {
+                res.status(400).json(err)
+            })
+    }
+}
